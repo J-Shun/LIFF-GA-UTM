0d371efbdf9c6be7c8b00f5b3cecebe160c151d6
Shun_Tseng <shun_tseng@migocorp.com>
2024-02-21 15:52:47 +0800

    feature: 新增 LIFF 初始化及 HTTPS 設置

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { useState, useEffect } from 'react';
+import liff from '@line/liff';
 import { sendDataToGtm } from './helper';
 import config from './constant/config';
 import reactLogo from './assets/react.svg';
@@ -9,13 +10,35 @@ function App() {
   const [count, setCount] = useState(0);
 
   useEffect(() => {
-    // sendDataToGtm({ event: 'setLineUid', key: 'lineUid', value: lineUid })
-    sendDataToGtm({
-      event: 'setGaTrackingId',
-      key: 'gaTrackingId',
-      value: config.GA4_TRACKING_ID,
-    });
-  });
+    const initLiff = async () => {
+      try {
+        await liff.init({ liffId: '2003704225-2ApWgyz8' });
+
+        // 若無登入，則跳制登入頁面
+        if (!liff.isLoggedIn()) {
+          liff.login();
+          return;
+        }
+
+        console.log('is Login', liff.isLoggedIn());
+
+        const profile = await liff.getProfile();
+        const lineUid = profile.userId;
+
+        // 配合 GTM 那邊做的特殊設置
+        sendDataToGtm({ event: 'setLineUid', key: 'lineUid', value: lineUid });
+        sendDataToGtm({
+          event: 'setGaTrackingId',
+          key: 'gaTrackingId',
+          value: config.GA4_TRACKING_ID,
+        });
+      } catch (error) {
+        alert('error');
+        console.log(error);
+      }
+    };
+    initLiff();
+  }, []);
 
   return (
     <>
--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,15 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import fs from 'fs';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: '/LIFF-GA-UTM/',
+  server: {
+    https: {
+      key: fs.readFileSync('./.cert/key.pem'),
+      cert: fs.readFileSync('./.cert/cert.pem'),
+    },
+  },
   plugins: [react()],
 });
