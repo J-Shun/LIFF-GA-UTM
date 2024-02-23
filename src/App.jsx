c73c5cfe7b8318d7990a7c1bff0f9d3025d3c27f
Shun_Tseng <shun_tseng@migocorp.com>
2024-02-23 16:57:49 +0800

    feature: 新增 utm 處理

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,37 @@
 import { useEffect } from 'react';
 import liff from '@line/liff';
-import { sendDataToGtm } from './helper';
+import {
+  sendDataToGtm,
+  parseQueryString,
+  setSessionStorage,
+  getSessionStorage,
+} from './helper';
 import config from './constant/config';
 import reactLogo from './assets/react.svg';
 import viteLogo from '/vite.svg';
 import './App.css';
 
 function App() {
+  const utmString = getSessionStorage('utm');
+  const utm = JSON.parse(utmString);
+
   useEffect(() => {
     const initLiff = async () => {
       try {
         await liff.init({ liffId: '2003704225-2ApWgyz8' });
+
+        const { search } = window.location;
+        const queryString = parseQueryString(search);
+        const { utm_source, utm_medium, utm_campaign, utm_term, utm_content } =
+          queryString;
+        const utm = {
+          utm_source: utm_source ?? '',
+          utm_medium: utm_medium ?? '',
+          utm_campaign: utm_campaign ?? '',
+          utm_term: utm_term ?? '',
+          utm_content: utm_content ?? '',
+        };
+        setSessionStorage('utm', JSON.stringify(utm));
 
         // 若無登入，則跳制登入頁面
         if (!liff.isLoggedIn()) {
@@ -47,6 +68,11 @@ function App() {
         </a>
       </div>
       <h1>GA & UTM</h1>
+      <div>{utm.utm_source}</div>
+      <div>{utm.utm_medium}</div>
+      <div>{utm.utm_campaign}</div>
+      <div>{utm.utm_term}</div>
+      <div>{utm.utm_content}</div>
     </>
   );
 }
--- src/helper/index.js
+++ src/helper/index.js
@@ -1,3 +1,31 @@
+/* localstorage 獲得、設定、移除 */
+function getLocalStorage(name) {
+  return localStorage.getItem(name);
+}
+function setLocalStorage(name, value) {
+  localStorage.setItem(name, value);
+}
+function clearLocalStorage(name) {
+  localStorage.removeItem(name);
+}
+function clearAllLocalStorage() {
+  localStorage.clear();
+}
+
+/* sessionstorage 獲得、設定、移除 */
+function getSessionStorage(name) {
+  return sessionStorage.getItem(name);
+}
+function setSessionStorage(name, value) {
+  sessionStorage.setItem(name, value);
+}
+function clearSessionStorage(name) {
+  sessionStorage.removeItem(name);
+}
+function clearAllSessionStorage() {
+  sessionStorage.clear();
+}
+
 /**
  * 將特殊資料傳送到 GTM dataLayer
  * @param {Object} options
@@ -12,4 +40,24 @@ function sendDataToGtm({ event, key, value }) {
   });
 }
 
-export { sendDataToGtm };
+function parseQueryString(queryString) {
+  const params = new URLSearchParams(queryString);
+  const obj = {};
+  for (const [key, value] of params) {
+    obj[key] = value;
+  }
+  return obj;
+}
+
+export {
+  getLocalStorage,
+  setLocalStorage,
+  clearLocalStorage,
+  clearAllLocalStorage,
+  getSessionStorage,
+  setSessionStorage,
+  clearSessionStorage,
+  clearAllSessionStorage,
+  sendDataToGtm,
+  parseQueryString,
+};
